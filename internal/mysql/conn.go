ac96873bcac0d9aad7185eab37557741f7948477
chscz <chs106@naver.com>
2024-09-13 16:18:41 +0900

    add db connection retry

--- internal/mysql/conn.go
+++ internal/mysql/conn.go
@@ -1,7 +1,9 @@
 package mysql
 
 import (
+	"log"
 	"net"
+	"time"
 
 	"github.com/chscz/backend-onboard-task/internal/config"
 	"github.com/go-sql-driver/mysql"
@@ -24,11 +26,22 @@ func InitMySQL(cfg config.MySQL) (*gorm.DB, error) {
 		ParseTime:            true,
 	}
 
+	var db *gorm.DB
+	var err error
+	retryCount := 20
 	dsn := mysqlCfg.FormatDSN()
-	db, err := gorm.Open(gormmysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
+	for i := 0; i < retryCount; i++ {
 
-	return db, nil
+		db, err = gorm.Open(gormmysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			return db, nil
+		}
+
+		log.Printf("Failed to connect to MySQL. Retrying (%d/%d)...\n", i+1, retryCount)
+		time.Sleep(time.Second * 5)
+		if i == retryCount-1 {
+			panic(err)
+		}
+	}
+	panic(err)
 }
